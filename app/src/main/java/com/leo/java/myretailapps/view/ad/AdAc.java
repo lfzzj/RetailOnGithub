a92f78fc98b8d82652795c5c5fa6756ba205cc97
Mrqin <1620504364@qq.com>
2019-03-15 23:44:34 +0800

    ###笔记本提交###

--- app/src/main/java/com/leo/java/myretailapps/view/ad/AdAc.java
+++ app/src/main/java/com/leo/java/myretailapps/view/ad/AdAc.java
@@ -1,4 +1,15 @@
 package com.leo.java.myretailapps.view.ad;
 
-public class AdAc {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 公告
+ */
+public class AdAc extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
